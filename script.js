c9de9b703939211a015679ed4bb5785d10d194d7
lis <lis@localhost.localdomain>
2022-01-01 18:09:00 +0900

    class/function test

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
+class Circle {
+    constructor(x, y, size, color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+    draw(color = '', is_update_color = false) {
+        if (color.length) {
+            console.log(`c ${color}`)
+            fill(color);
+            if (is_update_color) {
+                this.color = color;
+            }
+        }
+        else if (this.color.length) {
+            console.log(`c ${this.color}`)
+            fill(this.color);
+        }
+        ellipse(this.x, this.y, this.size);
+        this.log();
+    }
+    log(prefix = '') {
+        console.log(`${prefix} x, y, size = ${this.x}, ${this.y}, ${this.size}, ${this.color}`);
+    }
+}
+
+const COLOR_BG = '#000';
+
+let o;
+
 function setup() {
     //キャンバスを作成
     createCanvas(400, 600);
     //背景色
-    background(0);
+    background(COLOR_BG);
+    
+    o = new Circle(width / 2, height / 2, 50, '#A00');
 }
 
 function draw() {
-    background(0);
-    let x = width / 2;
-    let y = height / 2;
-    let size = 100;
-    fill(0, 255, 0);
-    ellipse(x, y, size);
+    background(COLOR_BG);
+    fill('#0A0');
+    // 図形描画
+    ellipse(0, 0, 50);
+    ellipse(0, 100, 50);
+    o.draw();
 }
 
+function e() {
+    ellipse(100, 400, 50);
+}
+
+function randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
